d546500f73bf2b127b37e625b0c842b202751659
Blair <abrgroup@gmail.com>
2020-02-15 20:48:29 -0500

    added styling

--- github-usercard/src/App.js
+++ github-usercard/src/App.js
@@ -2,6 +2,20 @@ import React from "react";
 import "./App.css";
 import axios from "axios";
 import FollowerList from "./FollowerList";
+import Styled from "styled-components";
+
+const Cards = Styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+`;
+const Card = Styled.div`
+  margin: 2rem 6rem;
+  padding: 1rem;
+  background-color: #D3D3D3;
+  box-shadow: 2px 2px #000000;
+  border-radius: 1.5rem;
+`;
 
 class App extends React.Component {
   state = {
@@ -35,12 +49,21 @@ class App extends React.Component {
     return (
       <div className="App">
         <h1>Blair's Github User Dashboard</h1>
-        <p>
-          <img src={this.state.profile.avatar_url} />
-        </p>
-        <p>Github Handle: {this.state.profile.login}</p>
-        <p>Github Profile: {this.state.profile.html_url}</p>
-        <p>Followers: {this.state.profile.followers}</p>
+        <Cards>
+          <Card>
+            <p>
+              <img src={this.state.profile.avatar_url} />
+            </p>
+            <p>Github Handle: {this.state.profile.login}</p>
+            <p>
+              Github Profile:{" "}
+              <a href={this.state.profile.html_url}>
+                {this.state.profile.html_url}
+              </a>
+            </p>
+            <p>Followers: {this.state.profile.followers}</p>
+          </Card>
+        </Cards>
         <FollowerList followers={this.state.followers} />
         {/* {!this.state.error &&
           this.state.followers.map(followers => ({
--- github-usercard/src/FollowerList.js
+++ github-usercard/src/FollowerList.js
@@ -1,20 +1,36 @@
 import React from "react";
+import Styled from "styled-components";
+
+const Cards = Styled.div`
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
+`;
+const Card = Styled.div`
+  margin: 2rem 6rem;
+  padding: 1rem;
+  background-color: #D3D3D3;
+  box-shadow: 2px 2px #000000;
+  border-radius: 1.5rem;
+`;
 
 const FollowerList = props => {
   return (
-    <div className="follower-list">
+    <Cards>
+      {/* <div className="follower-list"> */}
       {props.followers.map(item => (
-        <>
+        <Card>
           <p>
-            <img src={item.avatar_url} width="200px" />
+            <img src={item.avatar_url} width="200px" width="200px" />
           </p>
           <p>Github Handle: {item.login}</p>
           <p>
             Github Profile: <a href={item.html_url}>{item.html_url}</a>
           </p>
-        </>
+        </Card>
       ))}
-    </div>
+    </Cards>
+    // </div>
   );
 };
 
